8e9726ec24d2b08f189406de7c4d66b10f87cae0
Chukwu Matthew <matthewnfvcb@gmail.com>
2021-02-15 15:24:54 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7cd38c82ab832a41045ca633fb62e570'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '572ef1ab-10c1-4af3-8150-4f4f9ffcb9cd'
+};
